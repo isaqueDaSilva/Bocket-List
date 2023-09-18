feffc54e9f0e95bc40f0e7b57a7d4ca2a2a872c7
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-18 18:40:37 -0300

    Inital Commit

---
+++ BocketList/BocketList/BocketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BocketListApp.swift
+//  BocketList
+//
+//  Created by Isaque da Silva on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BocketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BocketList/BocketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BocketList
+//
+//  Created by Isaque da Silva on 18/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
