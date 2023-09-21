c2509a31c341301600caedd16268c61d66f837ad
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-20 23:55:51 -0300

    learning some things

--- BocketList/BocketList/ContentView.swift
+++ BocketList/BocketList/ContentView.swift
@@ -5,16 +5,20 @@
 //  Created by Isaque da Silva on 18/09/23.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Map(coordinateRegion: $mapRegion).ignoresSafeArea()
+            
+            Image(systemName: "mappin")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.red)
+                .frame(width: 44, height: 44)
         }
-        .padding()
     }
 }
